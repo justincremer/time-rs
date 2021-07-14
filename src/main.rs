f3c823c325fa607b98720e8624cf0f195bb626b5
justincremer <jacremer@live.com>
2021-07-13 18:20:56 -0700

    intial + finished

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::env;
+use std::io::{stderr, stdout, Write};
+use std::process::{exit, Command};
+use std::time::Instant;
+
+const MAN_PAGE: &'static str = /* @MANSTART{time} */
+    r#"
+NAME
+    time - timer for commands
+
+SYNOPSIS
+    time [ -h | --help ][COMMAND] [ARGUEMENT]...
+
+DESCRIPTION
+    Runs the command taken as the first arguement and outputs the time the command took to execute.
+
+OPTIONS
+    -h
+    --help
+        display this help and exit
+"#; /* @MANEND */
+
+fn main() {
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = stderr();
+
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        0 | 1 => {
+            let _ = writeln!(stderr, "{}", "Please provide a program name");
+            exit(1);
+        }
+        length => match args[1].as_str() {
+            "-h" | "--help" => {
+                let _ = writeln!(stdout, "{}", MAN_PAGE);
+                exit(0);
+            }
+            _ => {
+                let mut command = Command::new(&args[1]);
+                if length > 2 {
+                    for arg in &args[2..] {
+                        command.arg(arg);
+                    }
+                }
+
+                let time = Instant::now();
+                if let Err(e) = command.spawn() {
+                    let _ = writeln!(stderr, "{}", e);
+                    exit(1);
+                }
+                //     Ok(_) => {
+                //         let duration = time.elapsed();
+                //         let _ = writeln!(
+                //             stdout,
+                //             "{}m{:.3}s\n",
+                //             duration.as_secs() / 60,
+                //             (duration.as_secs() % 60) as f64
+                //                 + (duration.subsec_nanos() as f64) / 1000000000.0
+                //         );
+                //         exit(0);
+                //     }
+                //     Err(e) => {
+                //         let _ = writeln!(stderr, "{}", e);
+                //         exit(1);
+                //     }
+                // }
+
+                let duration = time.elapsed();
+                let _ = writeln!(
+                    stdout,
+                    "\nTook {}m {:.3}s",
+                    duration.as_secs() / 60,
+                    (duration.as_secs() % 60) as f64
+                        + (duration.subsec_nanos() as f64) / 1000000000.0
+                );
+                exit(0);
+            }
+        },
+    };
+}
